a36ddc0f0a3ee8524f4c3cec36238e055472bd3b
Scott Routledge <scottroutledge@scotts-mbp-5.lan>
2023-12-14 15:34:18 -0500

    function examples

---
+++ examples/function.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def multiply2(A, B):
+    return A @ B
+
+
+x = np.random.rand(3, 2)
+y = np.random.rand(2, 3)
+
+# error types of A and B should match up
+B = multiply2(x, y)
+
--- examples/loop_w_labels.py
+++ examples/loop_w_labels.py
@@ -1,7 +1,7 @@
 import torch
 
 # 100 batches of size 16 of 3 by 2 tensors
-data = [(torch.rand(16,3,32,32), torch.rand(1)) for _ in range(100)]
+data = [(1, torch.rand(16,3,32,32)) for _ in range(100)]
 
 W = torch.rand(31,32)
 
